f810f4189502bbd919360c8d7322a01ebd71a339
Dhruva <rdhruva12345@gmail.com>
2025-09-22 20:52:28 +0530

    feat: add docker

--- ecommerce-backend/src/app.module.ts
+++ ecommerce-backend/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
-import { OrderModule } from './order/order.module';
 import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [AuthModule, CategoryModule, ProductModule, OrderModule, CartModule],
+  imports: [AuthModule, CategoryModule, ProductModule, CartModule],
 })
 export class AppModule {}
