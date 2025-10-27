14638166037d4748a06f40d079f7d3e2f9532db0
Dhruva <rdhruva12345@gmail.com>
2025-10-27 13:40:03 +0530

    chore: add role based decorator

--- ecommerce-backend/src/auth/auth.controller.ts
+++ ecommerce-backend/src/auth/auth.controller.ts
@@ -1,29 +1,11 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Post,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
-import { AuthGuard } from './guards/auth.guard';
-import { AuthService } from './auth.service';
+import { Post } from '@nestjs/common';
+import { Role } from '@prisma/client';
+import { Roles } from 'src/common/roles/roles.decorator';
 
-@Controller('auth')
-export class AuthController {
-  constructor(private authService: AuthService) {}
-
-  @HttpCode(HttpStatus.OK)
-  @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.signIn(signInDto.username, signInDto.password);
-  }
-
-  @UseGuards(AuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
+export class Authcontroller {
+  @Post()
+  @Roles(Role.ADMIN)
+  create() {
+    return 'this is a test';
   }
 }
--- ecommerce-backend/src/auth/auth.service.ts
+++ ecommerce-backend/src/auth/auth.service.ts
@@ -4,21 +4,4 @@ import { JwtService } from '@nestjs/jwt';
 import { prisma } from 'libs/primsa';
 
 @Injectable()
-export class AuthService {
-  constructor(private jwtService: JwtService) {}
-
-  // async signIn(email: string, password: string) {
-  //   const user = await prisma.user.findUnique({
-  //     where: { email },
-  //     include: { credentials: true },
-  //   });
-
-  //   if (!user || user.credentials?.password !== password) {
-  //     throw new UnauthorizedException('Invalid credentials');
-  //   }
-  //   const payload = { sub: user.id, email: user.email };
-  //   return {
-  //     access_token: this.jwtService.sign(payload),
-  //   };
-  // }
-}
+export class AuthService {}
