36cf3a305c54b7833f272c7d88da88fa61718eaa
Dhruva <rdhruva12345@gmail.com>
2025-10-31 13:09:13 +0530

    chore: remove unnecessary comment and import

--- ecommerce-backend/src/app.module.ts
+++ ecommerce-backend/src/app.module.ts
@@ -5,8 +5,6 @@ import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
 import { UserModule } from './user/user.module';
 import { SellerModule } from './seller/seller.module';
-import { RolesGuard } from './common/roles/roles.guard';
-import { AuthGuard } from './auth/guards/auth.guard';
 
 @Module({
   imports: [AuthModule, ProductModule, CartModule, UserModule, SellerModule],
