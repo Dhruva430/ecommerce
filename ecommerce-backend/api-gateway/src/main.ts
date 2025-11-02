e476f6bef10eb768afa0ddfb252bcfcec47803bd
Dhruva <rdhruva12345@gmail.com>
2025-11-02 12:37:22 +0530

    feat: setup distributed service

---
+++ ecommerce-backend/api-gateway/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ZodValidationPipe } from '../../shared/libs/pipes/zod-validation-pipe';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Global Zod Validation
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 Service running on port ${port}`);
+}
+bootstrap();
---
+++ ecommerce-backend/shared/libs/pipes/zod-validation-pipe.ts
@@ -0,0 +1,23 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { ZodError, ZodType } from 'zod';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema?: ZodType<any, any>) {}
+
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    if (!this.schema) return value;
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException(error.issues);
+      }
+      throw new BadRequestException('Validation failed');
+    }
+  }
+}
