2c86bc33f8ba14a59cd8049e2d5adca76689bd93
Dhruva <rdhruva12345@gmail.com>
2025-11-08 11:27:00 +0530

    feat: add global shared libs

--- ecommerce-backend/services/api-gateway/src/app/app.service.ts
+++ ecommerce-backend/services/api-gateway/src/app/app.service.ts
@@ -1,8 +1,11 @@
+import { shared } from '@ecommerce-backend/shared';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
+    const data = shared();
+    console.log('Shared module says:', data);
     return { message: 'Hello API' };
   }
 }
--- ecommerce-backend/services/user-service/src/app/app.service.ts
+++ ecommerce-backend/services/user-service/src/app/app.service.ts
@@ -1,8 +1,10 @@
+import { shared } from '@ecommerce-backend/shared';
 import { Injectable } from '@nestjs/common';
-
 @Injectable()
 export class AppService {
   getData(): { message: string } {
+    const secret = shared();
+    console.log('Shared secret:', secret);
     return { message: 'Hello API' };
   }
 }
