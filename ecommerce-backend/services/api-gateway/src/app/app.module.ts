d7dbf4284f0a76ff732cce469148e17ccdc724c2
Dhruva <rdhruva12345@gmail.com>
2025-11-21 11:45:40 +0530

    feat: initial nest/js setup

---
+++ ecommerce-backend/services/api-gateway/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.TCP,
+        options: { host: 'localhost', port: 4001 },
+      },
+    ]),
+  ],
+  controllers: [AppController],
+})
+export class AppModule {}
